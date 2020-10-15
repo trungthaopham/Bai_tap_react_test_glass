27f27bb18e44868d08c550ebf3f0c04d130fd87f
phamthao27 <trungthao020797@gmail.com>
2020-10-15 17:36:11 +0700

    done-v2

--- src/BaiTapKinh/index.js
+++ src/BaiTapKinh/index.js
@@ -32,17 +32,19 @@ export default class BaiTapThuKinh extends Component {
 
     render() {
         return (
+
             <div className="" style={{ backgroundImage: 'url(./img/background.jpg)', width: '100%', height: '100%', position: 'fixed', top: '0', left: '0', backgroundRepeat: 'none', backgroundSize: 'cover' }}>
-                <div className="w-100 py-3 bg-dark text-white">
-                    <h2 className="display-"> TRY GLASSES APP ONLINE</h2>
+                <div className="position-absolute" style={{width: '100%', height: '100%', backgroundColor:'#8B888F80'}}></div>
+                <div className="w-100 py-3 bg-dark text-white" style={{position:'relative',zIndex:5}}>
+                    <h2 className="display-4"> TRY GLASSES APP ONLINE</h2>
                 </div>
-                <div className="container">
+                <div className="container position-relative">
                     <div className="row py-4">
-                        <div className="col-6" >
-                            <div className="col-8 w-100 position-relative p-0 item">
+                        <div className="col-6">
+                            <div className="col-8 w-100 position-relative p-0 item mx-auto">
                                 <img src="./img/model.jpg" className="img-fluid "></img>
                                 <img src={this.state.product.url} className="position-absolute" style={{ top: '26%', left: '50%', width: '169px', transform: 'translateX(-50%)' }}></img>
-                                <div className="position-absolute bg-dark text-white p-2 w-100" style={{ bottom: '0', left: '0' }}>
+                                <div className="position-absolute text-white p-2 w-100" style={{ bottom: '0', left: '0',backgroundColor:'#D98E3990' }}>
                                     <h4 className="display-5">{this.state.product.name}</h4>
                                     <p>{this.state.product.desc}</p>
 
@@ -50,7 +52,7 @@ export default class BaiTapThuKinh extends Component {
                             </div>
                         </div>
                         <div className="col-6" >
-                            <div className="col-8 w-100 item">
+                            <div className="col-8 w-100 item mx-auto">
                                 <img src="./img/model.jpg" className="img-fluid position-relative"></img>
                             </div>
                         </div>
@@ -61,6 +63,8 @@ export default class BaiTapThuKinh extends Component {
 
                 </div>
             </div>
+                
+
         )
     }
 }
